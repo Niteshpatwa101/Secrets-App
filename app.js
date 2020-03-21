068aeb05bf5692539adc0473c176f97de82f84ae
Nitesh Patwa <patwanitesh1997@gmail.com>
2020-03-21 20:41:10 +0530

    Level 1 - Username and Password Only

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +11,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended : true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB",
+    {useNewUrlParser : true,
+    useUnifiedTopology : true}
+);
+
+const userSchema = {
+    email : String,
+    password : String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -21,6 +34,34 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email : req.body.username ,
+        password : req.body.password
+    });
+    newUser.save((err) => {
+        if(!err){
+            res.render("secrets");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email : username}, (err, foundUser) => {
+        if(err){
+            console.log(err);
+        } else {
+            if (foundUser.password === password){
+                res.render("secrets");
+            }
+        }
+    });
 });
 
 app.listen(3000, () => {
