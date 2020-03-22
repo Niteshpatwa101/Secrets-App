ad1b26ad033ab7fa65ea87c19c37ed79e25e14f9
Nitesh Patwa <patwanitesh1997@gmail.com>
2020-03-22 15:24:35 +0530

    Level 7 - Logout and Completed SecretApp

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
     email : String,
     password : String,
-    googleId : String
+    googleId : String,
+    secret : String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -104,11 +105,39 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
+    User.find({"secret" : {$ne : null}}, (err, foundUsers) => {
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUsers){
+                res.render("secrets.ejs", {usersWithSecrets : foundUsers});
+            }
+        }
+    });
+});
+
+app.get("/submit", (req, res) => {
     if(req.isAuthenticated()){
-        res.render("secrets.ejs");
+        res.render("submit.ejs");
     } else{
         res.render("/login");
     }
+});
+
+app.post("/submit", (req, res) => {
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, (err, foundUser) => {
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(() => {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
 
 app.get("/logout", (req, res) => {
